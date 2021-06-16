4f924ff64f0750965fe15c92933be1ac3818cf78
aananya27 <astream.h@outlook.com>
2021-06-17 00:29:23 +0530

    feat: simple DB querying

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from flask import Flask, request
 from ingestor import youtube_ingestor
 from functools import partial
 from pymongo import MongoClient
+from bson.json_util import dumps
+import json
 
 client = MongoClient('mongodb://localhost:27017/')
 db = client['youtube']
@@ -20,12 +22,17 @@ app = Flask(__name__)
 def videos():
     offset = int(request.args.get('offset', default=0))
     size = int(request.args.get('size', default=30))
-    q = request.args.get('q')
+    q = request.args.get('q', default="")
+    res = []
+    if q == "":
+        db_result = video_collection.find({}).skip(offset).limit(size)
+        res= json.loads(dumps(list(db_result)))
+    else:
+        db_result = video_collection.find({"$text": {"$search": q}}).skip(offset).limit(size)
+        res = json.loads(dumps(list(db_result)))
 
     return {
-        'data': [
-
-        ],
+        'data': res,
         'page': {
             'size': size,
             'offset': offset,
